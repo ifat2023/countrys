4ade2a91849c824176f701044ad2e471a99e0eef
ifat2023 <ifat.elgart@gmail.com>
2023-05-28 21:09:04 +0300

    first commit

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,8 +12,8 @@ for (let i = 0; i < data.country.length; i++) {
   <div class="btn-group m-2" role="group" aria-label="Basic mixed styles example">
   <button type="button" onclick="predictInfo('${data.country[i].country_id}')"
    class="btn btn-danger">${data.country[i].country_id}</button>
-  <button type="button" class="btn btn-warning">${data.country[i].probability}</button>
-   </div> <br>
+  <button type="button" class="btn btn-warning">${(data.country[i].probability * 100).toFixed(2)}%</button>
+   </div><br> 
   `
 }
  document.querySelector("#countryList").innerHTML =str;
@@ -25,23 +25,48 @@ for (let i = 0; i < data.country.length; i++) {
   });
 
 
-}
+ }
 
-async function predictInfo(country) {
+// async function predictInfo(country) {
 
-fetch(`https://restcountries.com/v3.1/alpha/${country}`)
-  .then(response => response.json())
-  .then(data => {
+// fetch(`https://flagcdn.com/en/codes.json`)
+//   .then(response => response.json())
+//   .then(data => {
    
-    const flagImg = document.querySelector('imageElement').innerHTML
-    flagImg.src = data[0].flags.png;
+//     const flagImg = document.querySelector('imageElement').innerHTML
+//     flagImg.src = data[0].flags.png;
     
-  })
-  .catch(error => {
+//   })
+//   .catch(error => {
    
+//     console.log('There was an error', error);
+//   });
+//}
+
+const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+const countryCode = 'US'; // Replace with the desired country code
+
+const apiUrl = `https://flagsapi.com/#countries${countryCode}?access_key=${apiKey}`;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const flagUrl = data.flags[0];
+    
+   
+    const flagImg = document.createElement('img');
+    flagImg.src = flagUrl;
+    flagImg.alt = `${countryCode} Flag`;
+
+    
+    const imageElement = document.querySelector('#imageElement');
+    imageElement.appendChild(flagImg);
+  })
+  .catch((error) => {
     console.log('There was an error', error);
   });
 
 
-}
 
+
+    
