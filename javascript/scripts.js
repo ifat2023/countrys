cb63174ef508698009d85c301db78c89006fb65a
ifat2023 <ifat.elgart@gmail.com>
2023-05-29 11:40:54 +0300

    second commit

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -13,7 +13,7 @@ for (let i = 0; i < data.country.length; i++) {
   <button type="button" onclick="predictInfo('${data.country[i].country_id}')"
    class="btn btn-danger">${data.country[i].country_id}</button>
   <button type="button" class="btn btn-warning">${(data.country[i].probability * 100).toFixed(2)}%</button>
-   </div><br> 
+   </div> <br> 
   `
 }
  document.querySelector("#countryList").innerHTML =str;
@@ -27,44 +27,45 @@ for (let i = 0; i < data.country.length; i++) {
 
  }
 
-// async function predictInfo(country) {
+ async function predictInfo(country) {
 
-// fetch(`https://flagcdn.com/en/codes.json`)
-//   .then(response => response.json())
-//   .then(data => {
-   
-//     const flagImg = document.querySelector('imageElement').innerHTML
-//     flagImg.src = data[0].flags.png;
+  fetch(`https://flagcdn.com/en/codes.json`)
+    .then(response => response.json())
+    .then(data => {
+      const countryName = data[country.toLowerCase()];
+     
+      document.querySelector("#countryInfo").innerHTML= countryName 
+
+      flagImg.src = document.getElementById("imageElement").src;
+  src="https://flagsapi.com/AD/flat/64.png;"
+      flagImg.src = flagUrl;
+      flagImg.alt = `${countryCode} Flag`;
+
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('There was an error', error);
+    });
+}
+
+
+
+
+// fetch('https://flagsapi.com/TR/flat/64.png')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const flagUrl = data.flags[0];
     
+
+
+
+    
+//     const imageElement = document.querySelector('#imageElement');
+//     imageElement.appendChild(flagImg);
 //   })
-//   .catch(error => {
-   
+//   .catch((error) => {
 //     console.log('There was an error', error);
 //   });
-//}
-
-const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
-const countryCode = 'US'; // Replace with the desired country code
-
-const apiUrl = `https://flagsapi.com/#countries${countryCode}?access_key=${apiKey}`;
-
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    const flagUrl = data.flags[0];
-    
-   
-    const flagImg = document.createElement('img');
-    flagImg.src = flagUrl;
-    flagImg.alt = `${countryCode} Flag`;
-
-    
-    const imageElement = document.querySelector('#imageElement');
-    imageElement.appendChild(flagImg);
-  })
-  .catch((error) => {
-    console.log('There was an error', error);
-  });
 
 
 
